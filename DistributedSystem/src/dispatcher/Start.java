5b0494e02647475933c60c62257716f374e3516d
Cristian <Cristian@Cristian-PC>
2016-11-14 21:03:41 +0200

    Adding Start

---
+++ DistributedSystem/src/dispatcher/Start.java
@@ -0,0 +1,13 @@
+package dispatcher;
+
+import dispatcher.Dispatcher.EventTypeEnum;
+
+public class Start implements EventManager{
+
+	@Override
+	public void executeEvent(EventTypeEnum event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
